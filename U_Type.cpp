31f4f6b522566da18f52c6c715115f859d413f0d
kareems394 <112937239+kareems394@users.noreply.github.com>
2023-07-01 20:52:45 +0300

    Add files via upload

---
+++ U_Type.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+unsigned int pc;
+unsigned char memory[(16 + 64) * 1024];
+unsigned int reg[31];
+const unsigned int reg0 = 0;
+
+#include <iostream>
+
+using namespace std;
+
+void U_Type(unsigned int instWord) {
+    unsigned int rd, rs1, rs2, funct3, funct7, opcode;
+    unsigned int I_imm, S_imm, B_imm, U_imm, J_imm;
+    unsigned int address;
+
+    opcode = instWord & 0x0000007F;
+    rd = (instWord >> 7) & 0x0000001F;
+    funct3 = (instWord >> 12) & 0x00000007;
+    rs1 = (instWord >> 15) & 0x0000001F;
+    rs2 = (instWord >> 20) & 0x0000001F;
+
+    U_imm = ((instWord >> 12) & 0x7FFFF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
+    if(rd !=0){
+    switch (opcode) {
+        case 55:{
+            cout << "\tLUI\tx" << rd << ", 0x" << std::hex<< signed(U_imm) << "\n";
+            reg[rd-1] = U_imm <<12;
+            cout << reg[rd-1];
+            break;
+        }
+        case 23:{
+            cout << "\tAUIPC\tx" << rd << ", 0x" << std::hex<< signed(U_imm) << "\n";
+            reg[rd-1] = U_imm <<12 + pc;
+            break;
+        }
+    }
+    }
+    else{
+        cout << "\tLUI\tx" << reg0 << ", 0x" << std::hex<< signed(U_imm) << "\n";
+    }
+}
+
+int main() {
+    unsigned int instWord = 0x12334037;
+    U_Type(instWord);
+    return 0;
+}
