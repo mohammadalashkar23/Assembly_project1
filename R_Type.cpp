60b64ddc79e041ec88860e7c757010ae558d6f4b
kareems394 <112937239+kareems394@users.noreply.github.com>
2023-07-01 20:54:27 +0300

    Add files via upload

---
+++ R_Type.cpp
@@ -0,0 +1,125 @@
+/*
+	This is just a skeleton. It DOES NOT implement all the requirements.
+	It only recognizes the RV32I "ADD", "SUB" and "ADDI" instructions only.
+	It prints "Unkown Instruction" for all other instructions!
+
+	References:
+	(1) The risc-v ISA Manual ver. 2.1 @ https://riscv.org/specifications/
+	(2) https://github.com/michaeljclark/riscv-meta/blob/master/meta/opcodes
+*/
+
+#include <iostream>	
+#include <fstream>
+#include "stdlib.h"
+#include <iomanip>
+
+using namespace std;
+
+unsigned int pc;
+unsigned char memory[(16 + 64) * 1024];
+int reg[31];
+const unsigned int reg0 = 0;
+
+//R type
+void R_Type(unsigned int instWord)
+{
+	unsigned int rd, rs1, rs2, funct3, funct7, opcode;
+	unsigned int I_imm, S_imm, B_imm, U_imm, J_imm;
+	unsigned int address;
+
+	unsigned int instPC = pc - 4;
+
+	opcode = instWord & 0x0000007F;
+	rd = (instWord >> 7) & 0x0000001F;
+	funct3 = (instWord >> 12) & 0x00000007;
+	rs1 = (instWord >> 15) & 0x0000001F;
+	rs2 = (instWord >> 20) & 0x0000001F;
+	funct7 = (instWord >> 25) & 0x0000007F;
+
+	switch(funct3){
+		case 0:
+			if(funct7 == 32) {
+				cout << "\tSUB\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd-1] = reg[rs1 - 1] - reg[rs2 - 1];
+			}
+			else if(funct7 == 0) {
+				cout << "\tADD\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd-1] = reg[rs1 - 1] + reg[rs2 - 1];
+			}
+			else {
+				cout << "\tUnkown B Instruction \n";
+			}
+			break;
+
+		case 1:
+			{
+				cout << "\tSLL\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd-1] = reg[rs1-1] << reg[rs2-1];
+			}
+			break;
+
+		case 2:
+			{
+				cout << "\tSLT\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd - 1] = (reg[rs1 - 1] < reg[rs2 - 1]) ? 1 : 0;
+			}
+			break;
+
+		case 3:
+			{
+				cout << "\tSLTU\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd - 1] = (static_cast<unsigned int>(reg[rs1 - 1]) < static_cast<unsigned int>(reg[rs2 - 1])) ? 1 : 0;
+			}
+			break;
+
+		case 4:
+			{
+				cout << "\tXOR\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd-1] = reg[rs1-1] ^ reg[rs2-1];
+			}
+			break;
+
+		case 5:
+			if(funct7 == 32) {
+				cout << "\tSRA\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd-1] = reg[rs1-1] >> reg[rs2-1];
+			}
+			else if(funct7 == 0) {
+				cout << "\tSRL\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd-1] = static_cast<int>(static_cast<unsigned int>(reg[rs1 - 1]) >> static_cast<unsigned int>(reg[rs2 - 1]));
+			}
+			else {
+				cout << "\tUnkown B Instruction \n";
+			}
+			break;
+
+		case 6:
+			{
+				cout << "\tOR\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd-1] = reg[rs1-1] | reg[rs2-1];
+			}
+			break;
+
+		case 7:
+			{
+				cout << "\tAND\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+				reg[rd-1] = reg[rs1-1] & reg[rs2-1];
+			}
+			break;
+
+		default:
+			cout << "\tUnkown B Instruction \n";
+
+	}
+
+}
+
+
+int main() {
+
+	unsigned int instWord = 0b00000000001000001111100000110011;
+	reg[0]= 1; //rs1
+	reg[1]= 0; //rs2
+	R_Type(instWord);
+	cout<< "the output is: " << reg[15]<<endl;
+}
