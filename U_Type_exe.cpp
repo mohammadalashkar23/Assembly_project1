6449abff3474f13506cbdc1ec6d6f18de4019549
kareems394 <112937239+kareems394@users.noreply.github.com>
2023-07-02 22:59:11 +0300

    Updated_U_Type_exe.cpp

--- U_Type_exe.cpp
+++ U_Type_exe.cpp
@@ -23,24 +23,22 @@ void U_Type(unsigned int instWord) {
     rs2 = (instWord >> 20) & 0x0000001F;
 
     U_imm = ((instWord >> 12) & 0x7FFFF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
-
     switch (opcode) {
         case 55:{
-            cout << "\tLUI\tx" << rd << ", 0x" << std::hex<< U_imm << "\n";
             reg[rd-1] = U_imm <<12;
             cout << reg[rd-1];
             break;
         }
         case 23:{
-            cout << "\tAUIPC\tx" << rd << ", 0x" << std::hex<< U_imm << "\n";
             reg[rd-1] = U_imm <<12 + pc;
             break;
         }
     }
+    
 }
 
 int main() {
-    unsigned int instWord = 0b00000000111000010010010000110111;
+    unsigned int instWord = 0x12334037;
     U_Type(instWord);
     return 0;
 }
